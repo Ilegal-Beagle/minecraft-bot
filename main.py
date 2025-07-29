d2ab7191a78fa5ae129996c12d328d9594ed651e
Ilegal-Beagle <munoza204@gmail.com>
2025-07-28 23:37:28 -0500

    added to .gitignore, block finding, pathing, and mining for logs work better now!

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ bot = mineflayer.createBot({
     'hideErrors': False,
     'version':'1.21',
 })
+mc_data = require('minecraft-data')(bot.version)
+
+LOGS = [
+    'acacia_log',
+    'birch_log',
+    'cherry_log',
+    'dark_oak_log',
+    'jungle_log',
+    'mangrove_log',
+    'oak_log',
+    'spruce_log']
+LOGS = [mc_data.blocksByName[log].id for log in LOGS]
 
 bot.loadPlugin(pathfinder.pathfinder)
 print("Started mineflayer")
@@ -27,49 +39,58 @@ def handle(*args):
 
     @On(bot, 'chat')
     def handleMsg(this, sender, message, *args):
-        if sender and (sender != BOT_USERNAME):
+        if not sender and (sender == BOT_USERNAME):
+            return
 
-            if 'come' in message:
-                player = bot.players[sender]
-                target = player.entity
+        if 'come' in message:
+            player = bot.players[sender]
+            target = player.entity
 
-                if not target:
-                    bot.chat("I don't see you !")
-                    return
+            if not target:
+                bot.chat("I don't see you !")
+                return
 
-                pos = target.position
-                bot.pathfinder.setMovements(movements)
-                go_to(pos)
+            pos = target.position
+            bot.pathfinder.setMovements(movements)
+            go_to(pos)
 
-            elif 'go to entity' in message:
-                entity = bot.nearestEntity()
-                if not entity:
-                    bot .chat('I dont see you!')
-                    return
+        elif 'go to entity' in message:
+            entity = bot.nearestEntity()
+            if not entity:
+                bot .chat('I dont see you!')
+                return
 
-                pprint(entity)
-                pos = entity.position
-                bot.pathfinder.setMovements(movements)
-                bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, RANGE_GOAL))
+            pprint(entity)
+            pos = entity.position
+            bot.pathfinder.setMovements(movements)
+            go_to(pos)
 
-            elif 'look at me' in message:
-                sender_pos = bot.players[sender].entity['position']
-                bot.lookAt(sender_pos.offset(0, 1, 0))
+        elif 'look at me' in message:
+            sender_pos = bot.players[sender].entity['position']
+            bot.lookAt(sender_pos.offset(0, 1, 0))
 
-            elif 'look for new tree' in message:
-                go_to_nearest_log()
+        elif 'mine logs' in message:
+            # find one log nearby and go to it 
+            blocks = find_blocks(LOGS, block_count=1)
+            blocks = [bot.blockAt(block) for block in blocks]
 
-            elif 'l' in message:
-                mine_nearby_logs()
+            if not blocks:
+                bot.chat('ion see any doggone trees \'round these parts')
+                return
+            
+            pos = blocks[0]['position']
+            bot.pathfinder.setMovements(movements)
+            go_to(pos)
 
+            # find all reachable blocks at tree and mine it
+            blocks = find_blocks(LOGS, 5, 64)
+            blocks = [bot.blockAt(block) for block in blocks]
+            blocks = [block for block in blocks if bot.canDigBlock(block)]
+            for block in blocks:
+                bot.dig(block,True)
+                print(block.position)                
+                bot.chat('mined all nearby logs!')
 
-@On(bot, 'diggingCompleted')
-def handle(block, *args):
-    print(f'block broken')
-
-@On(bot, 'health')
-def handle(*args):
-    bot.chat('ouch!!')
 
 @On(bot, 'end')
 def handle(*args):
@@ -78,39 +99,11 @@ def handle(*args):
 def go_to(pos):
     bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, RANGE_GOAL))
 
-
-def find_blocks(block_identifier:str=None, search_area:int=512) -> list:
-    blocks = bot.findBlocks(
+def find_blocks(block_id:list=None, search_area:int=512, block_count:int=512) -> list:
+    return bot.findBlocks(
         {
-            'point':bot.position,
-            'matching':lambda block: block.name != 'air',
-            'count': search_area
+            'point':bot.entity.position.offset(0,1,0),
+            'matching': block_id,
+            'maxDistance':search_area,
+            'count': block_count
         })
-    
-    blocks = [bot.blockAt(pos) 
-              for pos in blocks if block_identifier in bot.blockAt(pos).name]
-    return blocks
-
-def go_to_nearest_log(search_area=512):
-    blocks = find_blocks('log',search_area)
-    
-    # pathfind to nearest log in list
-    if blocks:
-        pos = blocks[0]
-        bot.pathfinder.setGoal(
-            pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, RANGE_GOAL))
-    else:
-        bot.chat('ion see any doggone trees \'round these parts')
-
-def mine_nearby_logs(search_area:int=512):
-    blocks = find_blocks('log')    
-    
-    if blocks:
-        for block in blocks:
-            if bot.canDigBlock(block):
-                bot.dig(block,True)
-                print(block.position)
-        
-        bot.chat('mined all nearby logs!')
-    else:
-        bot.chat('ion see any doggone trees \'round these parts')
